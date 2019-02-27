ff0b05734b1df73a74cf3d964453bf99ff91e315
Erin McBride <erinmcbride@Hannahs-Air.attlocal.net>
2019-02-26 18:30:12 -0600

    added ticTacToe project

---
+++ DOM-ticTacToe/scripts/ticTacToe.js
@@ -0,0 +1,15 @@
+var gameMarker = "x"
+
+function changeMarkerToX(){
+    var gameMarker = "0"
+    console.log("The O button was clicked!")
+}
+
+function changeMarkerToO(){
+    var gameMarker = "X"
+    console.log("The X button was clicked!")
+}
+
+function placeMark(){
+    console.log("The div button was clicked!")
+}
