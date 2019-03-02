6bb85c6d8165457b86b80951ea6064305b373a0e
Erin McBride <erinmcbride@Hannahs-MacBook-Air.local>
2019-03-02 12:50:48 -0600

    updated todo list with jQuery

--- dashboard/todo.js
+++ dashboard/todo.js
@@ -1,1 +1,18 @@
-alert("Hello! I am an alert box!!");
+$(function() {
+
+    let $list = $('ul');
+    let $newItemForm = $('#newItemForm');
+  
+    $newItemForm.on('submit', function(e) {
+      e.preventDefault();
+      let text = $('input[type="text"]').val();
+      $list.append(`<li>${text}</li>`);
+      $('input[type="text"]').val('');
+    });
+  
+    $list.on('click', 'li', function() {
+      let $this = $(this);
+      $this.remove();
+    });
+  
+  });
