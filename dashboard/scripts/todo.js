6f2aca16a6cd6b3d8a0f2782431cfa990ef72d8a
Erin McBride <erinmcbride@Hannahs-Air.attlocal.net>
2019-03-05 22:39:14 -0600

    added location button to bassboard, but entire project needs a redesign

---
+++ dashboard/scripts/todo.js
@@ -0,0 +1,34 @@
+$(function() {
+  let $list = $("ul");
+  let $newItemForm = $("#newItemForm");
+
+  $newItemForm.on("submit", function(e) {
+    e.preventDefault();
+    let text = $('input[type="text"]').val();
+    $list.append(`<li>${text}</li>`);
+    $('input[type="text"]').val("");
+  });
+
+  $list.on("click", "li", function() {
+    let $this = $(this);
+    $this.remove();
+  });
+});
+
+var myLocation = document.getElementById("my-coordinates");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayPosition);
+  } else {
+    myLocation.innerHTML = "Not available";
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML =
+    "Longitude: " +
+    position.coords.longitude +
+    " Latitude: " +
+    position.coords.latitude;
+}
