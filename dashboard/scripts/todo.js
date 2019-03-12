5822765cfa2d0ec13ec8ecc94011cf6fa66cf417
Erin McBride <erinmcbride@Hannahs-Air.attlocal.net>
2019-03-12 00:21:25 -0500

    added stress reliever to dashboard project

--- dashboard/scripts/todo.js
+++ dashboard/scripts/todo.js
@@ -34,3 +34,14 @@ function displayPosition(position) {
     " Latitude: " +
     position.coords.latitude;
 }
+
+// Stress Reliever
+
+let clicks = 0;
+
+function revealMessage() {
+    document.getElementById("hiddenText").style.display = "block";
+    clicks++;
+    console.log(clicks);
+    document.getElementById("newText").innerHTML = 'Current number of clicks: ' + clicks;
+}
